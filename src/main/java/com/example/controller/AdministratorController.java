04c08a21f24bbc74b33fcdb42d0fb1fbd6f1d5de
igamasayuki <igamasayuki@gmail.com>
2023-04-19 17:04:46 +0900

    first commit

---
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -0,0 +1,109 @@
+package com.example.controller;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Administrator;
+import com.example.form.InsertAdministratorForm;
+import com.example.form.LoginForm;
+import com.example.service.AdministratorService;
+
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * 管理者情報を操作するコントローラー.
+ * 
+ * @author igamasayuki
+ *
+ */
+@Controller
+@RequestMapping("/")
+public class AdministratorController {
+
+	@Autowired
+	private AdministratorService administratorService;
+
+	@Autowired
+	private HttpSession session;
+
+	/////////////////////////////////////////////////////
+	// ユースケース：管理者を登録する
+	/////////////////////////////////////////////////////
+	/**
+	 * 管理者登録画面を出力します.
+	 * 
+	 * @param form フォーム
+	 * @return 管理者登録画面
+	 */
+	@GetMapping("/toInsert")
+	public String toInsert(InsertAdministratorForm form) {
+		return "administrator/insert";
+	}
+
+	/**
+	 * 管理者情報を登録します.
+	 * 
+	 * @param form 管理者情報用フォーム
+	 * @return ログイン画面へリダイレクト
+	 */
+	@PostMapping("/insert")
+	public String insert(InsertAdministratorForm form) {
+		Administrator administrator = new Administrator();
+		// フォームからドメインにプロパティ値をコピー
+		BeanUtils.copyProperties(form, administrator);
+		administratorService.insert(administrator);
+		return "redirect:/";
+	}
+
+	/////////////////////////////////////////////////////
+	// ユースケース：ログインをする
+	/////////////////////////////////////////////////////
+	/**
+	 * ログイン画面を出力します.
+	 * 
+	 * @param form フォーム
+	 * @return ログイン画面
+	 */
+	@GetMapping("/")
+	public String toLogin(LoginForm form) {
+		return "administrator/login";
+	}
+
+	/**
+	 * ログインします.
+	 * 
+	 * @param form   管理者情報用フォーム
+	 * @param result エラー情報格納用オブジェクト
+	 * @return ログイン後の従業員一覧画面
+	 */
+	@PostMapping("/login")
+	public String login(LoginForm form, Model model) {
+		Administrator administrator = administratorService.login(form.getMailAddress(), form.getPassword());
+		if (administrator == null) {
+			model.addAttribute("errorMessage", "メールアドレスまたはパスワードが間違っています");
+			return toLogin(form);
+		}
+		session.setAttribute("administratorName", administrator.getName());
+		return "redirect:/employee/showList";
+	}
+
+	/////////////////////////////////////////////////////
+	// ユースケース：ログアウトをする
+	/////////////////////////////////////////////////////
+	/**
+	 * ログアウトをします. (SpringSecurityに任せるためコメントアウトしました)
+	 * 
+	 * @return ログイン画面
+	 */
+	@GetMapping(value = "/logout")
+	public String logout() {
+		session.invalidate();
+		return "redirect:/";
+	}
+
+}
